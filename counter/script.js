d0f74a575d7cb8983a9430197d21fdd5163225d0
Masaharu-Fuji <pencil68872@gmail.com>
2025-05-20 22:01:59 +0900

    Modify counter

--- counter/script.js
+++ counter/script.js
@@ -9,14 +9,34 @@ const addEventClickButton = (id) => {
 
 const calcCounter = (id, target) => {
   if (id === 'increment-button') {
-    counter.textContent = target + 1;
+    target += 1;
+  } else if (id === 'increment-10-button') {
+    target += 10;
   } else if (id === 'decrement-button') {
-    counter.textContent = target - 1;
+    target -= 1;
+  } else if (id === 'decrement-10-button') {
+    target -= 10;
+  } else if (id === 'multiplication-button') {
+    target *= 2;
   } else {
-    counter.textContent = 0;
+    target = 0;
+  }
+  counter.textContent = target;
+  echoText(50, target);
+}
+
+const echoText = (checker, target) => {
+  const text = document.querySelector('.text');
+  if (target > checker) {
+    text.textContent = `${checker}を超えました！`;
+  } else {
+    text.textContent = ``;
   }
 }
 
 addEventClickButton('increment-button');
 addEventClickButton('decrement-button');
+addEventClickButton('increment-10-button');
+addEventClickButton('decrement-10-button');
+addEventClickButton('multiplication-button');
 addEventClickButton('reset-button');
