70e0e377c996d8588204a21a2ae2ba6be558c01f
Masaharu-Fuji <pencil68872@gmail.com>
2025-05-20 21:30:44 +0900

    Improvement counter

--- counter/script.js
+++ counter/script.js
@@ -1,6 +1,22 @@
 const counter = document.getElementById('counter-number');
 
-document.getElementById('increment-button').addEventListener('click', () => {
-  let currentCount = parseInt(counter.textContent);
-  counter.textContent = currentCount + 1;
-});
+const addEventClickButton = (id) => {
+  document.getElementById(id).addEventListener('click', () => {
+    let currentCount = parseInt(counter.textContent);
+    calcCounter(id, currentCount);
+  });
+}
+
+const calcCounter = (id, target) => {
+  if (id === 'increment-button') {
+    counter.textContent = target + 1;
+  } else if (id === 'decrement-button') {
+    counter.textContent = target - 1;
+  } else {
+    counter.textContent = 0;
+  }
+}
+
+addEventClickButton('increment-button');
+addEventClickButton('decrement-button');
+addEventClickButton('reset-button');
