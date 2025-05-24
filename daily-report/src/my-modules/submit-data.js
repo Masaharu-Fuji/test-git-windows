61d20eee777fb415860f242f48bd4f7900d03008
Masaharu-Fuji <pencil68872@gmail.com>
2025-05-24 21:23:08 +0900

    Modify addDoc input default

--- daily-report/src/my-modules/submit-data.js
+++ daily-report/src/my-modules/submit-data.js
@@ -7,9 +7,9 @@ export const submitData = async (e, db) => {
   try {
     const docRef = await addDoc(collection(db, "reports"), {
       date: new Date(),
-      name: formData.get("name"),
-      work: formData.get("work"),
-      comment: formData.get("comment")
+      name: formData.get("name") || "No Name",
+      work: formData.get("work") || "",
+      comment: formData.get("comment") || ""
     });
     console.log("Document written with ID: ", docRef.id)
   } catch (e) {
