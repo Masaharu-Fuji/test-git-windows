d86cdf4c848d445801a2cc624acbac4d61ddca5e
Masaharu-Fuji <pencil68872@gmail.com>
2025-05-24 18:46:23 +0900

    Add config firebase

---
+++ daily-report/src/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUl-T9Phq-vBeeGd9wVIpMOThVg7JK5TA",
+  authDomain: "daily-report-87de7.firebaseapp.com",
+  projectId: "daily-report-87de7",
+  storageBucket: "daily-report-87de7.firebasestorage.app",
+  messagingSenderId: "732030484041",
+  appId: "1:732030484041:web:ea9c50a778e54578430998"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
